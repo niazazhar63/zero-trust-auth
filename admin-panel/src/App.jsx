105572ecbe417386aba8c9d6ef42f5e30743e802
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-08-29 22:38:59 +0600

    admin panel created

---
+++ admin-panel/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import PendingPage from './pages/PendingPage'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
