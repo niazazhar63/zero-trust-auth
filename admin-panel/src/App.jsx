97173ee6c1a50a3c56fea6cce48cb37e7da2d305
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-08-30 22:35:30 +0600

    admin login functionality added

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,33 +1,48 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
-import { Route,Routes } from 'react-router-dom'
-import AllRequests from './pages/AllRequests'
-import Add from "./pages/Add"
-import AllAccount from './pages/AllAccount'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import AllRequests from "./pages/AllRequests";
+import Add from "./pages/Add";
+import AllAccount from "./pages/AllAccount";
+import { useEffect, useState } from "react";
+import Login from "./components/Login";
+import { Toaster } from "react-hot-toast";
+
+
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 function App() {
+  const [token, setToken] = useState(localStorage.getItem(`token`)?localStorage.getItem('token'): "");
+
+  useEffect(()=>{
+    localStorage.setItem('token', token)
+  },[token])
 
   return (
-    <div className='bg-gray-50 min-h-screen'>
-      <Navbar />
+    <div className="bg-gray-50 min-h-screen">
+      <Toaster position="top-right" />
 
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar />
 
-      <div className='flex w-full'>
-        <Sidebar />
-        <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base'>
-
-        <Routes>
-          <Route path='/allRequests' element={<AllRequests />}></Route>
-          <Route path='/addEmail' element={<Add />}></Route>
-          <Route path='/allUsers' element={<AllAccount />}></Route>
-
-        </Routes>
-        </div>
-      </div>
-
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/allRequests" element={<AllRequests />}></Route>
+                <Route path="/addEmail" element={<Add />}></Route>
+                <Route path="/allUsers" element={<AllAccount />}></Route>
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
